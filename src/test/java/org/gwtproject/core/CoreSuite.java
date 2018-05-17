2bc0cef898f3e506a48018e5ca9f25470f39cbcc
Colin Alworth <colin@colinalworth.com>
2018-05-17 07:28:51 -0500

    Added more tests from upstream gwt

--- src/test/java/org/gwtproject/core/CoreSuite.java
+++ src/test/java/org/gwtproject/core/CoreSuite.java
@@ -2,9 +2,7 @@ package org.gwtproject.core;
 
 import com.google.gwt.junit.tools.GWTTestSuite;
 import junit.framework.Test;
-import org.gwtproject.core.client.JavaScriptObjectTest;
-import org.gwtproject.core.client.SchedulerTest;
-import org.gwtproject.core.client.ScriptInjectorTest;
+import org.gwtproject.core.client.*;
 import org.gwtproject.core.client.impl.SchedulerImplTest;
 
 public class CoreSuite {
@@ -12,6 +10,9 @@ public class CoreSuite {
         GWTTestSuite suite = new GWTTestSuite("All core tests");
 
         suite.addTestSuite(JavaScriptObjectTest.class);
+        suite.addTestSuite(JsArrayTest.class);
+        suite.addTestSuite(JsArrayMixedTest.class);
+        suite.addTestSuite(JsIdentityTest.class);
         suite.addTestSuite(SchedulerTest.class);
         suite.addTestSuite(SchedulerImplTest.class);
         suite.addTestSuite(ScriptInjectorTest.class);
